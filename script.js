858dd960500a0d758bd83f19e18aa7ce90abb5ce
David Arce <davidarce.dia@gmail.com>
2022-06-21 00:50:50 -0400

    popup modal complete and styled. Issue with 'submit' button

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ const $bookPageCount = document.querySelector('#bookPageCount');
 const $bookbookStatus = document.querySelector('#bookbookStatus');
 
 console.log($bookTitle.value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+const popUpButton = document.getElementById("popUpButton");
+const formPopUp = document.getElementById("formPopUp");
+const submitButtion = document.getElementById("addBookButton");
+
+popUpButton.addEventListener('click', () => {
+  formPopUp.classList.add('show');
+});
+
+submitButtion.addEventListener('click', () => {
+  formPopUp.classList.remove('show');
+});
