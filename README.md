9a0f7df86b1c2b38d8d57dea5648720f6e4d4d3b
David Arce <davidarce.dia@gmail.com>
2022-07-10 03:03:51 -0400

    addBookToLibrary function creates a book object, adds it to an array, and cals a function 'displayBook' to display to the DOM.

--- README.md
+++ README.md
@@ -5,9 +5,17 @@ Header containing logo and Sign In/Sign Up button
 Add Book button
 -When pressed, a popup asks for book title, author, page count, and status (read/not read)
 
+When the user fills out the new book info, a book object is created
+
 Each book object goes into an array
 
 Each book object is displayed on the screen
+-In order from top to bottom:
+  -Title
+  -Author
+  -PageCount
+  -ReadStatus
+    -Users can toggle the ReadStatus
 
 Books can be removed by pressing a "Delete" button. 
 
--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-/*
-let myLibrary = [
-  {title: "book" , author: "book" , pageCount: "book" , status: "Read"}
-];
+const myLibrary = [];
 
-
+//Book Object Constructor
 function Book(title, author, pageCount, status) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.status = status;
-}
-
-function addBookToLibrary() {
-
-}
-
-const $bookTitle = document.querySelector('#bookTitle');
-const $bookAuthor = document.querySelector('#bookAuthor');
-const $bookPageCount = document.querySelector('#bookPageCount');
-const $bookbookStatus = document.querySelector('#bookbookStatus');
-
-console.log($bookTitle.value)
-
-
-*/
-
-
-
-
-
-
-
-
+} 
 
 //Selectors
 const modalButton = document.getElementById("modalButton");
@@ -40,11 +15,15 @@ const overlay = document.getElementById("overlay");
 const addBookButton = document.getElementById("addBookButton");
 
 //Event Listeners
-modalButton.addEventListener('click', () => {
+modalButton.addEventListener('click', () => { 
   modal.classList.add('active');
   overlay.classList.add('active');
 });
 
+//adding books is done from here
+document.addEventListener('DOMContentLoaded', ()=>{
+  addBookButton.addEventListener('click', addBookToLibrary);
+})
 
 overlay.addEventListener('click', () => {
   modal.classList.remove('active');
@@ -55,4 +34,58 @@ addBookButton.addEventListener('click', () => {
   modal.classList.remove('active');
   overlay.classList.remove('active');
 });
+
 //Functions
+//Pushes a new book to the end of the myLibrary array
+const addBookToLibrary = () => {
+
+  //Prevents form from refreshing
+  event.preventDefault();
+
+  //Selectors
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pageCount = document.getElementById('pageCount');
+  const readStatus = document.getElementById('readStatus');
+
+  //Creates a new book object and adds it to the array
+  const book = new Book(title.value, author.value, pageCount.value, readStatus.value);
+  console.log(book);
+
+  myLibrary.push(book);
+  console.log(myLibrary);
+
+  //Form is cleared without refreshing the page
+  document.querySelector('form').reset();
+
+  displayBook(myLibrary);
+}
+
+
+const displayBook= (myLibrary) => {
+ 
+
+  if(myLibrary !== undefined){
+
+      //creating book div container
+      const bookDiv = document.createElement("div");
+      bookDiv.classList.add("book");
+
+      //creating the title, author, pagecount, and status elements.
+      const titleDiv = document.createElement("div");
+      titleDiv.innerText = '"' + myLibrary[(myLibrary.length-1)].title + '"';
+      bookDiv.appendChild(titleDiv);
+
+      const authorDiv = document.createElement("div");
+      authorDiv.innerText = "by" + myLibrary[(myLibrary.length-1)].author;
+      bookDiv.appendChild(authorDiv);
+
+      const pageCountDiv = document.createElement("div");
+      pageCountDiv.innerText = myLibrary[(myLibrary.length-1)].pageCount + ' pages';
+      bookDiv.appendChild(pageCountDiv);
+
+      const main = document.querySelector('main');
+      main.appendChild(bookDiv);
+    
+  }
+}
